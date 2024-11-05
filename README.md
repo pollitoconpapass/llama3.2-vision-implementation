ff38041f9aa38faa2d78977c7ff83e02c2e9517c
Jose Quispe <jalbertoqz@gmail.com>
2024-11-05 00:30:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# LLAMA 3.2 Vision Implementation 🦙👁️
+
+Script to load and use LLAMA model for image analysis
+
+[!IMPORTANT]
+> The time execution will depend on your machine components. (RAM, GPU, etc)
+
+# 🗂️ Pre-requirements
+1. Hugging Face account
+2. Permission to the model. Request it [here](https://huggingface.co/meta-llama/Llama-3.2-11B-Vision)
+3. Hugging Face token
+
+# 🏛️ Structure
+- `imgs` -> Folder that contain the images to analyze🕶️
+- `api.py` -> Script that uses the LLAMA 3.2 Vision as an endpoint
+- `llama_vision.ipynb` -> Notebook implementation of the model
+ 
+
+# 🤖 Steps 2 Follow
+1. Install all the requirements
+
+        pip install -r requirements.txt
+
+2. Follow the Jupyter Notebook and run each cell
+
+3. If you want to test the `api.py`. Run:
+
+        python api.py
+
+[!NOTE]
+> You can download the Notebook and run it in a Colab environment. 
+
+
---
+++ api.py
@@ -0,0 +1,41 @@
+import torch 
+import uvicorn
+from PIL import Image
+from fastapi import FastAPI, File, UploadFile
+from transformers import (
+    MllamaForConditionalGeneration,
+    AutoProcessor,
+    GenerationConfig,
+)
+
+app = FastAPI()
+
+# === LOADING THE MODEL ===
+model_id = "meta-llama/Llama-3.2-11B-Vision" # -> model name
+model = MllamaForConditionalGeneration.from_pretrained(
+    model_id,
+    torch_dtype=torch.bfloat16,
+    device_map="auto",
+)
+# model.to('cpu')
+model.gradient_checkpointing_enable()
+processor = AutoProcessor.from_pretrained(model_id)
+
+
+# === ENDPOINT IMPLEMENTATION ===
+@app.post("/ask-llama-3-2-vision")
+def answer_image_question(prompt: str, image_file: UploadFile = File(...)):
+  image = Image.open(image_file)
+  prompt = f"<|image|><|begin_of_text|>{prompt}" # -> special prompt style
+
+  inputs = processor(image, prompt, return_tensors="pt").to("cpu") # -> assigned to the cpu (change it in case you have GPU machine)
+  generation_config = GenerationConfig.from_pretrained(model_id)
+  generation_config.gradient_checkpointing = True
+  output = model.generate(**inputs, generation_config=generation_config, 
+                          max_new_tokens=250)
+
+  return processor.decode(output[0])  # -> final response
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=4040)
